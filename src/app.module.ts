213926ba853b9896d6c44df6e9cd2423465ad485
DESKTOP-ESLIT23\pc <hcabrera@northware.mx>
2022-08-12 09:22:45 -0500

    migracion proyecto curso nest

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { EnvConfiguration } from './config/env.config';
+import { JoinValidationSchema } from './config/joi.validation';
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      validationSchema: JoinValidationSchema,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname,'..','public'),
+    }),
+    MongooseModule.forRoot(process.env.MONGODB),
+    PokemonModule,
+    CommonModule,
+    SeedModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {
+  constructor(
+    private readonly configService:ConfigService
+    ){}
+}
